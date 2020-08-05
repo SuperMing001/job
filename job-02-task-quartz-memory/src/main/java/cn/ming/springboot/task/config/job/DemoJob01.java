acacece3fbf19f1fa0c71e9b0d3b73c776862ef1
SuperMing001 <renxiaoming90@aliyun.com>
2020-08-05 10:20:39 +0800

    快速入门 Quartz 单机

---
+++ job-02-task-quartz-memory/src/main/java/cn/ming/springboot/task/config/job/DemoJob01.java
@@ -0,0 +1,34 @@
+package cn.ming.springboot.task.config.job;
+
+import cn.ming.springboot.task.service.DemoService;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author ren.xm
+ * @description
+ * @date 2020/8/5
+ */
+public class DemoJob01 extends QuartzJobBean {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final AtomicInteger counts = new AtomicInteger();
+
+    @Autowired
+    private DemoService demoService;
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        logger.info("[executeInternal][定时第 ({}) 次执行, demoService 为 ({})]", counts.incrementAndGet(),
+                demoService);
+
+    }
+
+}
---
+++ job-02-task-quartz-memory/src/main/java/cn/ming/springboot/task/config/job/DemoJob02.java
@@ -0,0 +1,23 @@
+package cn.ming.springboot.task.config.job;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+/**
+ * @author ren.xm
+ * @description
+ * @date 2020/8/5
+ */
+public class DemoJob02 extends QuartzJobBean {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        logger.info("[executeInternal][我开始的执行了]");
+    }
+
+}
